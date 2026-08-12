38d9a605569ac372132dcf432298237fd35705bc
Rivanes <143288384+Rivanes@users.noreply.github.com>
2026-08-12 07:57:44 +0200

    Delete src directory

--- src/platform/data/index.js
+++
@@ -1,8 +0,0 @@
-export { AdminContextRepository } from "./admin-context-repository.js";
-export { VenueRepository } from "./venue-repository.js";
-export { ExhibitionAdminRepository } from "./exhibition-admin-repository.js";
-export { SiteRepository } from "./site-repository.js";
-export { MediaAdminRepository } from "./media-admin-repository.js";
-export { UserRepository } from "./user-repository.js";
-export { AdminRepository } from "./admin-repository.js";
-export { RepositoryError } from "./base.js";
